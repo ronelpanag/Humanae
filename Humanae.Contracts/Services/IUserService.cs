5c2c5e281762c5f4d125e5946920769e36f2a1ae
Ronel Pantaleon Agramonte <rpantaleon@evopointsolution.com>
2021-06-26 21:27:46 -0400

    Lista de usuarios

--- Humanae.Contracts/Services/IUserService.cs
+++ Humanae.Contracts/Services/IUserService.cs
@@ -1,5 +1,7 @@
 ﻿using Humanae.DomainGlobal;
+using Humanae.Dto;
 using Humanae.Dto.Parameters;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Humanae.Contracts.Services
@@ -7,5 +9,6 @@ namespace Humanae.Contracts.Services
     public interface IUserService
     {
         Task<ServiceResult> Login(AuthenticateParameter parameter);
+        Task<ServiceResult<List<UserDto>>> GetAll();
     }
 }
--- Humanae/Views/UserListView.Designer.cs
+++ Humanae/Views/UserListView.Designer.cs
@@ -30,11 +30,60 @@ namespace Humanae.Views
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserListView));
+            this.label1 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(375, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Listado de Usuarios";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 158);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1114, 502);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // UserListView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UserListView";
+            this.ClientSize = new System.Drawing.Size(1113, 657);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "UserListView";
+            this.Text = "Humanae - Listado de Usuarios";
+            this.Load += new System.EventHandler(this.UserListView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
